8880c5b1f03f733be25d82524d946bc61fa0cffc
yousseflets <leticiayoussef.si@gmail.com>
2024-07-29 16:56:20 -0300

    angular17 listagem produtos

--- product-store/src/app/features/list/list.component.ts
+++ product-store/src/app/features/list/list.component.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
